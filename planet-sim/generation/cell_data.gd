9b87c3c1ff1bfe6972e48acab3b5129501344ab4
Kegrathil <willkegrathil@gmail.com>
2025-11-19 15:29:30 +0100

    directory housekeeping

---
+++ planet-sim/generation/cell_data.gd
@@ -0,0 +1,18 @@
+extends RefCounted
+class_name CellData
+
+var id : int
+var neighbours : Array[int]
+
+var unit_pos : Vector3
+
+var height : float = 0.0
+
+var plate_id : int = -1
+var is_oceanic : bool = false
+var stress_rotation_direction : Vector3
+var debug_neighbour_stress : float = 0.0
+
+func _init(cell_id : int, pos : Vector3) -> void:
+	id = cell_id
+	unit_pos = pos
